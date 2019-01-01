161055d82b08fe14d08775221873b1bbff00d51b
Manoj Sharma <Manoj Sharma>
2019-01-01 22:29:16 +0530

    Updates

---
+++ precache-manifest.ae35bb85f3b1906fabbf11b75d7b227b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6796c732547b6b7e19cdc8e4ad58356",
+    "url": "/weather-app/static/media/bg.b6796c73.jpg"
+  },
+  {
+    "revision": "2be6f7590b391fa0dcfc",
+    "url": "/weather-app/static/js/runtime~main.2be6f759.js"
+  },
+  {
+    "revision": "4444b89c893e3e36fa61",
+    "url": "/weather-app/static/js/main.4444b89c.chunk.js"
+  },
+  {
+    "revision": "babd94826ba891d51a02",
+    "url": "/weather-app/static/js/1.babd9482.chunk.js"
+  },
+  {
+    "revision": "4444b89c893e3e36fa61",
+    "url": "/weather-app/static/css/main.4ff60cab.chunk.css"
+  },
+  {
+    "revision": "6bda07c296afe48d9b0183d5dc3df077",
+    "url": "/weather-app/index.html"
+  }
+];
